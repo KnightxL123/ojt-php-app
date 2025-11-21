5e0754c0e4d495f1d7445f0becd90fab27ce5b78
KnightxL123 <153537013+KnightxL123@users.noreply.github.com>
2025-11-21 12:34:09 +0800

    Update header.php

--- ojt_web_app/shared/header.php
+++ ojt_web_app/shared/header.php
@@ -1,4 +1,6 @@
 <?php
+include 'config/DBconfig.php';
+
 $res = $conn->prepare("SELECT COUNT(*) AS unread FROM announcement_recipients WHERE recipient_id = ? AND is_read = 0");
 $res->bind_param('i', $_SESSION['user_id']);
 $res->execute();
@@ -12,3 +14,4 @@ $res->close();
         <span style="background: red; color: white; padding: 2px 6px; border-radius: 50%;"><?php echo $unread_count; ?></span>
     <?php endif; ?>
 </a>
+
