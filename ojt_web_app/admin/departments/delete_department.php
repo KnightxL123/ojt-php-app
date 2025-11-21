5a4a7d3f52282bb4168f2399639981b093f3f62e
KnightxL123 <153537013+KnightxL123@users.noreply.github.com>
2025-11-21 11:51:08 +0800

    Delete ojt_web_app directory

--- ojt_web_app/admin/departments/delete_department.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "your_database_name");
-if (isset($_GET['id'])) {
-    $id = intval($_GET['id']);
-    $conn->query("DELETE FROM departments WHERE id = $id");
-}
-header("Location: manage.php");
-exit;
